28b7b7b2f12a39be99dab8078013a232d96ca40b
ASOUDRY <soudryalex@gmail.com>
2023-02-19 22:38:02 +0000

    done

--- src/main/java/OrderResultSetActivity.java
+++ src/main/java/OrderResultSetActivity.java
@@ -43,6 +43,7 @@ public class OrderResultSetActivity {
      */
 
     public List<Character> problem1() {
+        ///. 
         /**
          * Problem 1: Write a statement below to query the database for all characters. Make sure the results are in
          * ascending order by last name, and first name as a tie-breaker.
